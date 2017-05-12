a1c165ea0757970c62aa100e9c792d4c89125b03
dany1468 <dany1468@gmail.com>
2017-05-13 07:17:57 +0900

    Add Wrap and Kikura-redis to package dependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "SidekiqPushClient"
+    name: "SidekiqPushClient",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1),
+        .Package(url: "https://github.com/JohnSundell/Wrap.git", majorVersion: 2),
+    ]
 )
