ff5b0ce24d9b457b271413f5bae71dd600a43629
dany1468 <dany1468@gmail.com>
2017-05-14 08:03:53 +0900

    Add SidekiqPushClient

--- Sources/SidekiqPushClient.swift
+++ Sources/SidekiqPushClient.swift
@@ -1,4 +1,36 @@
-struct SidekiqPushClient {
+import Foundation
+import Wrap
 
-    var text = "Hello, World!"
+public enum SidekiqPushError: Error {
+    case serializingJsonFailed
+}
+
+public struct SidekiqPushClient {
+    let redisClient: RedisClient
+
+    init(_ redisHost: String, _ redisPort: Int32) {
+        self.redisClient = RedisClient(redisHost, redisPort)
+    }
+
+    init() {
+        self.redisClient = RedisClient()
+    }
+
+    public func push(class: String, args: Array<Any> = [], queue: String = "default", retry: Int = 25) throws -> String {
+        let payload = Payload(class: `class`, args: args, queue: queue, retry: retry)
+
+        try redisClient.rawPush(queue, try dumpJson(payload))
+
+        return payload.jid
+    }
+
+    func dumpJson(_ payload: Payload) throws -> String {
+        let dictionary: [String: Any] = try! wrap(payload)
+        do {
+            let data = try JSONSerialization.data(withJSONObject: dictionary, options: [])
+            return String(data: data, encoding: .utf8)!
+        } catch {
+            throw SidekiqPushError.serializingJsonFailed
+        }
+    }
 }
--- Tests/SidekiqPushClientTests/SidekiqPushClientTests.swift
+++ Tests/SidekiqPushClientTests/SidekiqPushClientTests.swift
@@ -2,16 +2,15 @@ import XCTest
 @testable import SidekiqPushClient
 
 class SidekiqPushClientTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(SidekiqPushClient().text, "Hello, World!")
-    }
+    func testPush() {
+        let client = SidekiqPushClient()
 
+        try! client.push(class: "SampleWorker", args: [123, ["completed": true]])
+    }
 
     static var allTests : [(String, (SidekiqPushClientTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testPush", testPush),
         ]
     }
 }
